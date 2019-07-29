7111b07ed2f82359e9fb8179784bdc3bdf1f4677
Yuriy Puris <donovanyuriy@gmail.com>
2019-07-29 08:41:55 +0300

    client

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,24 +1,143 @@
-import React from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogActions from "@material-ui/core/DialogActions";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import DialogContentText from "@material-ui/core/DialogContentText";
-// import DialogTitle from "@material-ui/core/DialogTitle";
-// import Slide from "@material-ui/core/Slide";
-// import Gavel from "@material-ui/icons/Gavel";
-// import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
+import React, {useState} from "react";
+import {Mutation} from 'react-apollo'
+import {gql} from 'apollo-boost'
 
-const Register = ({ classes }) => {
-  return <div>Register</div>;
+import withStyles from "@material-ui/core/styles/withStyles";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import Gavel from "@material-ui/icons/Gavel";
+import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
+import { create } from "jss";
+
+function Transition(props) {
+  return <Slide direction="up" {...props}/>
+}
+
+const Register = ({ classes, setNewUser }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [open, setOpen] = useState(false);
+  
+  const handleSubmit = async (event, createUser) => {
+    event.preventDefault();
+    const res = await createUser();
+    console.log({res})
+    setOpen(true);    
+  }
+  
+  
+  return <div className={classes.root}>
+          <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <Gavel/>
+            </Avatar>
+            <Typography variant="headline">
+              Register
+            </Typography>
+            <Mutation 
+              mutation={REGISTER_MUTATION}
+              variables={{username, email, password}}
+              onCompleted={data => {
+                console.log({data})
+                setOpen(true)
+              }}>  
+              {
+                (createUser, {loading, error}) => {
+                  return (
+                    <form 
+                      className={classes.form}
+                      onSubmit={event => handleSubmit(event, createUser)}>
+                      <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="username">Username</InputLabel>
+                        <Input 
+                          id="username"
+                          onChange={event => setUsername(event.target.value)}/>
+                      </FormControl>
+                      <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="email">Email</InputLabel>
+                        <Input 
+                          id="email"
+                          onChange={event => setEmail(event.target.value)}/>
+                      </FormControl>
+                      <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <Input 
+                          id="password"
+                          onChange={event => setPassword(event.target.value)}/>
+                      </FormControl>
+                      <Button
+                        className={classes.submit}
+                        type="submit"
+                        variant="contained"
+                        color="secondary"
+                        disabled={loading || !username || !email || !password}
+                        fullWidth>
+                          {loading ? "Registering ..." : "Register"}
+                      </Button>
+                      <Button
+                        type="submit"
+                        variant="outlined"
+                        color="primary"
+                        fullWidth
+                        onClick={() => setNewUser(false)}>
+                          Previous user ? Log in here
+                      </Button>
+
+                      {error && <div>Error</div>}
+                    </form>  
+                  )
+                }
+              }
+            </Mutation>
+          </Paper>
+
+          <Dialog 
+            open={open}
+            disableBackdropClick={true}
+            TransitionComponent={Transition}>
+              <DialogTitle>
+                <VerifiedUserTwoTone className={classes.icon}/>
+                New Account
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  User succesfully creared!
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={() => setNewUser(false)}>
+                  Login
+                </Button>
+              </DialogActions>
+          </Dialog>  
+        </div>;
 };
+
+const REGISTER_MUTATION = gql`
+  mutation ($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      user {
+        username
+        email
+      }
+    }
+  }
+`
 
 const styles = theme => ({
   root: {
