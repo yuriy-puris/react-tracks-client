d99cda7060ba8fb3a238143fc14a418f7bdda2ff
Yuriy Puris <donovanyuriy@gmail.com>
2019-07-30 07:34:28 +0300

    add tracks

--- src/components/Track/CreateTrack.js
+++ src/components/Track/CreateTrack.js
@@ -1,21 +1,40 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogActions from "@material-ui/core/DialogActions";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import DialogContentText from "@material-ui/core/DialogContentText";
-// import DialogTitle from "@material-ui/core/DialogTitle";
-// import FormControl from "@material-ui/core/FormControl";
-// import FormHelperText from "@material-ui/core/FormHelperText";
-// import TextField from "@material-ui/core/TextField";
-// import Button from "@material-ui/core/Button";
-// import CircularProgress from "@material-ui/core/CircularProgress";
-// import AddIcon from "@material-ui/icons/Add";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import AddIcon from "@material-ui/icons/Add";
+import ClearIcon from "@material-ui/icons/Clear";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 const CreateTrack = ({ classes }) => {
-  return <div>CreateTrack</div>;
+  return (
+    <div>
+      <Button
+        variant="fab"
+        className={classes.fab}
+        color="secondary">
+          <AddIcon />
+      </Button>
+      <Dialog className={classes.dialog}>
+        <form>
+          <DialogTitle>Create Dialog</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Add a title, description and audiofile
+            </DialogContentText>
+          </DialogContent>
+        </form>
+      </Dialog>
+    </div>
+  )
 };
 
 const styles = theme => ({
